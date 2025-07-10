3efe07a48da26f4808ccb7fe08ba56ad6358e1f9
Fabiana R. <rossifabiana930@gmail.com>
2025-07-10 15:09:50 -0300

    Controladores terminados.

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -2,7 +2,7 @@ import languages from "../models/language.model.js";
 
 const isNameUnique = async (name) => {
     const language = await languages.findOne({ where: { name: name } });
-    return res.status(400).json({ error: "El nombre del lenguaje ya se encuentra en la base de datos." });
+    return character === null;
 };
 
 export const getAllLanguages = async (req, res) => {
@@ -18,7 +18,11 @@ export const getAllLanguages = async (req, res) => {
 
 export const getLanguageById = async (req, res) => {
     try {
+        if (!language){
+            return res.status(404).json({ error: "El id no se encuentra en la base de datos." });
+        };
         const language = await languages.findByPk(req.params.id);
+        res.status(200).json(language);
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
@@ -28,7 +32,15 @@ export const getLanguageById = async (req, res) => {
 
 export const createLanguage = async (req, res) => {
     try {
-        
+        const { name, paradigm, release_year } = req.body;
+        if (!name || !paradigm || !release_year){
+            return res.status(400).json({ error: "Faltan datos obligatorios." });
+        };
+        if (!(await isNameUnique(name))){
+            return res.status(400).json({ error: "El nombre del lenguaje ya existe en la base de datos." });
+        };
+        const language = await languages.create({ name, paradigm, release_year });
+        res.status(201).json(language);
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
@@ -38,7 +50,20 @@ export const createLanguage = async (req, res) => {
 
 export const updateLanguage = async (req, res) => {
     try {
-        
+        const { id } = req.params;
+        const { name, paradigm, release_year } = req.body;
+        const language = await languages.findByPk(id);
+        if (!name || !paradigm || !release_year){
+            return res.status(400).json({ error: "Faltan datos obligatorios." });
+        };
+        if (!(await isNameUnique(name))){
+            return res.status(400).json({ error: "El nombre del lenguaje ya existe en la base de datos." });
+        };
+        if (!language){
+            return res.status(404).json({ error: "El id no se encuentra en la base de datos." });
+        };
+        await languages.update(req.body);
+        res.status(200).json({ message: "El lenguaje se actualizó con éxito:" },language);
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
@@ -49,7 +74,13 @@ export const updateLanguage = async (req, res) => {
 
 export const deleteLanguage = async (req, res) => {
     try {
-        
+        const { id } = req.params;
+        const language = await languages.findByPk(id);
+        if (!language){
+            return res.status(404).json({ error: "No existe ese id en la base de datos." });
+        }
+        await languages.destroy();
+        res.status(200).json({ message: "El personaje se eliminó con éxito." });
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
