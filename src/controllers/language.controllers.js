a1ffd40293a5c9bab74ab3f827fed2cec94e4ba6
Fabiana R. <rossifabiana930@gmail.com>
2025-07-10 15:01:22 -0300

    Cambios en controllers.

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -1,8 +1,14 @@
 import languages from "../models/language.model.js";
 
+const isNameUnique = async (name) => {
+    const language = await languages.findOne({ where: { name: name } });
+    return res.status(400).json({ error: "El nombre del lenguaje ya se encuentra en la base de datos." });
+};
+
 export const getAllLanguages = async (req, res) => {
     try {
-        
+        const language = await languages.findAll();
+        res.status(200).json(language);
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
@@ -12,7 +18,7 @@ export const getAllLanguages = async (req, res) => {
 
 export const getLanguageById = async (req, res) => {
     try {
-        
+        const language = await languages.findByPk(req.params.id);
     } catch (error) {
         res.status(500).json({ error: "Error interno en el servidor." });
         console.log("-------------------------------------------------");
