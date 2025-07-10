f5f202fa2f99eada1c225d5a712a5b3d6781d3c1
Fabiana R. <rossifabiana930@gmail.com>
2025-07-10 14:41:16 -0300

    Database y models hecho.

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const database = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.PORT
+    }
+);
+
+export default database;
